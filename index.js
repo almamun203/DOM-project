4c1cb930c5862cc332f6f4a6edef37336d99c380
almamun203 <alauddinalmamun203@gmail.com>
2024-02-11 17:12:17 +0600

    added styles

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('lime').addEventListener('click',
+function(){
+    document.body.style.backgroundColor='lime';
+}
+)
+
+function makeCyan(){
+    document.body.style.backgroundColor='cyan';
+}
